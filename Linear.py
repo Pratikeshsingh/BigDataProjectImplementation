9462d80088331b95aba95123ebe0e8c158824975
Pratikesh Singh <pratikesh.singh@gmail.com>
2020-04-09 11:36:12 +0200

    final versions used in paper

--- Linear.py
+++ Linear.py
@@ -3,29 +3,21 @@ from pathlib import Path
 from PIL import Image
 from PIL import ImageFilter
 import sys
+import os
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
 
-in_path = Path.cwd() / "train_set"
+os.chdir('/Users/pratikeshsingh/Desktop/Masters/BigData/Parallel')
+in_path = Path.cwd() / "images"
 out_path = Path.cwd() / "processed"
-
-if not in_path.exists():
-    print(f"Cannot find {in_path.name} directory")
-    sys.exit(1)
-
-if not out_path.exists():
-    out_path.mkdir()
 
 import time
 start = time.time()
 names = list(in_path.glob("*"))
-for image_file in names:
-    outfile = out_path / image_file.name
-    try:
-        image = Image.open(image_file)
-        # image.thumbnail((128, 128))
-        image.filter(ImageFilter.DETAIL)
-        #image.save(outfile, "JPEG")
-    except IOError:
-        print(f"Cannot create thumbnail for {image_file.name}")
+for filename in names:
+    outfile = out_path / filename.name
+    image = plt.imread(filename)
+    gray = rgb2gray(image)
+    plt.imsave(outfile,gray,format="png")    
 
 print(time.time() - start)
-
--- MultiProcessing.py
+++ MultiProcessing.py
@@ -4,26 +4,20 @@ from PIL import Image
 from PIL import ImageFilter
 import sys
 import concurrent.futures
+import os
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
 
-in_path = Path.cwd() / "train_set"
+os.chdir('/Users/pratikeshsingh/Desktop/Masters/BigData/Parallel')
+in_path = Path.cwd() / "images"
 out_path = Path.cwd() / "processed"
-
-if not in_path.exists():
-    print(f"Cannot find {in_path.name} directory")
-    sys.exit(1)
-
-if not out_path.exists():
-    out_path.mkdir()
 
 def process_one_file(filename):
     outfile = out_path / filename.name
-    try:
-        image = Image.open(filename)
-        image.filter(ImageFilter.DETAIL)
-        #image.save(outfile, "JPEG")
-    except IOError:
-        print(f"Cannot create thumbnail for {filename.name}")
-
+    image = plt.imread(filename)
+    gray = rgb2gray(image)
+    plt.imsave(outfile,gray,format="png")
+    
 names = list(in_path.glob("*")) #* 4
 
 import time
@@ -31,6 +25,3 @@ start = time.time()
 executor = concurrent.futures.ThreadPoolExecutor(99)
 list(executor.map(process_one_file, names))
 print(time.time() - start)
-
-# for image_file in in_path.glob("*"):
-#     process_one_file(image_file)
